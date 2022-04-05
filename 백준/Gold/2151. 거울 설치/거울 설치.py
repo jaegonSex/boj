199f370831bdb7b57755074caf761a8e7b5179ff
IM JAE GON <89837407+jaegonSex@users.noreply.github.com>
2022-04-05 16:39:19 +0900

    [Gold IV] Title: 거울 설치, Time: 968 ms, Memory: 283236 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.py
+++ 백준/Gold/2151. 거울 설치/거울 설치.py
@@ -14,8 +14,6 @@ for i in range(N):
             else:
                 sx,sy = i, j
             tmp +=1
-
-
 def bfs(x, y, dir):
     global N
     result = math.inf
@@ -23,29 +21,21 @@ def bfs(x, y, dir):
     dy = [1, 0, -1, 0]
     queue = deque()
     queue.append((x,y,dir,0))
-
     while queue:
         nx, ny, ndir, ncount = queue.popleft()
         xx, yy= nx + dx[ndir], ny+ dy[ndir]
         if graph[nx][ny] == '#':
             if nx!= x or ny!=y:
                 result = ncount
-
         if 0<=xx< N and 0<=yy<N and ncount<result:
-
             if graph[xx][yy] =='.' or graph[xx][yy] =='#':
-
                 queue.appendleft((xx,yy,ndir,ncount))
-
             if graph[xx][yy] =='!':
-
                 queue.appendleft((xx,yy,ndir,ncount))
                 queue.append((xx,yy,(ndir+1)%4, ncount +1))
                 queue.append((xx,yy,(ndir+3)%4, ncount +1))
-
     return result
 result = math.inf
 for i in range(4):
     result = min(bfs(sx, sy, i),result)
-    result = min(bfs(ex,ey,i), result)
 print(result)
