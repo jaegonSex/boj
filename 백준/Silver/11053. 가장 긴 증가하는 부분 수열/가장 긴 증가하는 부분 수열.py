b6514ea51f248412399995608beec2d66468e7c7
IM JAE GON <89837407+jaegonSex@users.noreply.github.com>
2022-04-08 00:54:00 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 196 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = list(map(int,input().split()))
+dp = [1 for i in range(n)]
+for i in range(1,n):
+    for j in range(i):
+        if a[j]<a[i]:
+            dp[i] = max(dp[j] +1, dp[i])
+print(max(dp))
