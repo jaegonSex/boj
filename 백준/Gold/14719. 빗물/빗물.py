5729b8352413db2e8d69cd68d186ccc4965c6ff3
IM JAE GON <89837407+jaegonSex@users.noreply.github.com>
2022-04-20 01:41:12 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 32432 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,26 +1,14 @@
 import sys
-from collections import deque
+from collections import deque, Counter
 h, w = map(int, sys.stdin.readline().split())
 blocks = list(map(int, sys.stdin.readline().split()))
-graph = deque([deque([0 for i in range(w)]) for i in range(h)])
+graph = deque([deque(['0' for i in range(w)]) for i in range(h)])
 idx = 0
 for block in blocks:
     for i in range(block):
-        graph[i][idx] = 1
+        graph[i][idx] = '1'
     idx+=1
-def pop_side_zero(deq):
-    while True:
-        if deq and deq[0]==0:
-            deq.popleft()
-        if deq and deq[-1] ==0:
-            deq.pop()
-        if not deq or (deq[0] ==1 and deq[-1]==1):
-            break
-    return deq
-
-
-result = 0
+count = 0
 for i in graph:
-    result += sum([1 for i in pop_side_zero(i) if i == 0])
-
-print(result)
+    count +=Counter(''.join(i).rstrip('0').lstrip('0'))['0']
+print(count)
