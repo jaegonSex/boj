d49424071c6c0d332833c0d73b649890d764d6aa
IM JAE GON <89837407+jaegonSex@users.noreply.github.com>
2022-04-26 06:00:49 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 300 ms, Memory: 17856 KB -BaekjoonHub

--- 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
@@ -1,13 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.OptionalInt;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class Main {
 
 
@@ -15,15 +10,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
-        List<Integer> collect = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-            int minVal = collect.stream().mapToInt(c -> c).filter(c -> (c & 1) == 0).min().getAsInt();
-            int sum = collect.stream().mapToInt(c -> c).filter(c -> (c & 1) == 0).sum();
-            System.out.println(sum + " " + minVal);
+            IntSummaryStatistics collect = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt)
+                    .filter(c->(c&1) ==0).collect(Collectors.summarizingInt(c -> c));
+            System.out.println(collect.getSum() + " " + collect.getMin());
         }
-
-
     }
 }
-
-
-
